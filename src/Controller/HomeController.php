f0456fe09a7179991675dfa9b164734b2464b8af
Arthur Lucas-le guilly <arthurlucas2002@hotmail.com>
2022-05-06 08:53:09 +0200

    choix vehicule OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Model;
 use App\Entity\User;
+use App\Entity\Vehicle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,8 +27,11 @@ class HomeController extends AbstractController
      */
     public function reservation(): Response
     {
+        $lesVoitures = $this->getDoctrine()->getRepository(Vehicle::class);
+        $repo = $this->getDoctrine()->getRepository(Model::class);
+        $lesModels = $repo->findAll();
         return $this->render('home/reservation.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'HomeController', 'vehicle' => $lesVoitures, 'model' => $lesModels,
         ]);
     }
 }
